cecc6fa9ad845872667342dc25993a546d26954a
swati2904 <swati.lpu17@gmail.com>
2020-07-01 14:43:01 +0530

    added about us component and perform routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 //import { empty, from } from 'rxjs';
 import {DataBindingComponent} from './data-binding/data-binding.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
 const routes: Routes = [
   {path: '',component: HomeComponent},
-  {path: 'data-binding',component: DataBindingComponent}
+  {path: 'data-binding',component: DataBindingComponent},
+  {path: 'aboutus',component:AboutusComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import {FormsModule} from '@angular/forms';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
