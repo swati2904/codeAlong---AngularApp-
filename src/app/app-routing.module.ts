324501bd2776a8b244fa05a1cf1e5f8161e41c4e
swati2904 <swati.lpu17@gmail.com>
2020-07-01 14:19:20 +0530

    added the home,header and databinding component and perform the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+//import { empty, from } from 'rxjs';
+import {DataBindingComponent} from './data-binding/data-binding.component';
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'data-binding',component: DataBindingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import {FormsModule} from '@angular/forms';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
